ecbe9c2fdcad33a266b0776962bd99918b06ef56
FARDOUS Nacer Eddine <96303420+FNaouss@users.noreply.github.com>
2022-06-02 22:15:52 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  async function sendButton(e) {
+    e.preventDefault();
+    const response = await fetch({
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      Authorization:
+        "Bearer EAAKGt1fZCIE8BABZBuXObVgSrWZAjUgrCWyUfLU4JIx5c3cqUNrKmqeZB8FULHPSQLiqKPMUpm2ayrZAsXrAcDFzaWFPmmOi7vD5oDdQNS2OIWrwPCmX1FBkBboAFYXantiyLgp8VWAJOEIoBb9DW6RzyssU5H7diJUoogNyyNZBkZBc8fg82aitujihXUpUEJ3jNaxV5bM2wZDZD",
+      body: JSON.stringify({
+        messaging_product: "whatsapp",
+        to: phone,
+        text: { body: message },
+      }),
+    });
+  }
+
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+  const handlePhoneChange = (e) => {
+    setPhone(e.target.value);
+  };
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+  const handleFooterChange = (e) => {
+    setFooter(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={sendButton}>
+        <div>
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            onChange={handlePhoneChange}
+            required
+          ></input>
+        </div>
+        <div>
+          <label>Message</label>
+          <input
+            type="text"
+            name="message"
+            onChange={handleMessageChange}
+            required
+          ></input>
+        </div>
+        <div>
+          <button type="submit">Envoyer le message</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
